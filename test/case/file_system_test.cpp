318b11740abde6d0e7b9d6b821772a8b5ebec1bf
owt5008137 <owt5008137@live.com>
2017-04-12 22:44:15 +0800

    add expect for file_system unit test

--- test/case/file_system_test.cpp
+++ test/case/file_system_test.cpp
@@ -4,24 +4,24 @@
 CASE_TEST(file_system, dirname) {
     std::string dir;
 
-    util::file_system::dirname(__FILE__, 0, dir);
+    CASE_EXPECT_TRUE(util::file_system::dirname(__FILE__, 0, dir));
     CASE_MSG_INFO() << "Current dir: " << dir << std::endl;
 
-    util::file_system::dirname(__FILE__, 0, dir, 2);
+    CASE_EXPECT_TRUE(util::file_system::dirname(__FILE__, 0, dir, 2));
     CASE_MSG_INFO() << "Test dir: " << dir << std::endl;
 
-    util::file_system::dirname(__FILE__, 0, dir, 3);
+    CASE_EXPECT_TRUE(util::file_system::dirname(__FILE__, 0, dir, 3));
     CASE_MSG_INFO() << "Project dir: " << dir << std::endl;
 }
 
 CASE_TEST(file_system, scan_dir) {
     std::string dir;
-    util::file_system::dirname(__FILE__, 0, dir, 2);
+    CASE_EXPECT_TRUE(util::file_system::dirname(__FILE__, 0, dir, 2));
 
     std::list<std::string> out;
-    util::file_system::scan_dir(dir.c_str(), out, util::file_system::dir_opt_t::EN_DOT_DAFAULT | util::file_system::dir_opt_t::EN_DOT_ABSP |
-                                                      util::file_system::dir_opt_t::EN_DOT_RLNK |
-                                                      util::file_system::dir_opt_t::EN_DOT_RECU);
+    CASE_EXPECT_EQ(0, util::file_system::scan_dir(
+                          dir.c_str(), out, util::file_system::dir_opt_t::EN_DOT_DAFAULT | util::file_system::dir_opt_t::EN_DOT_ABSP |
+                                                util::file_system::dir_opt_t::EN_DOT_RLNK | util::file_system::dir_opt_t::EN_DOT_RECU));
 
     CASE_MSG_INFO() << "All files in " << dir << std::endl;
     for (std::list<std::string>::iterator iter = out.begin(); iter != out.end(); ++iter) {
