00e417e4d6e1601c78a52d236c319290e0906b05
owent <owt5008137@live.com>
2017-07-25 17:23:28 +0800

    prepare for crypto cipher

---
+++ include/algorithm/crypto_cipher.h
@@ -0,0 +1,141 @@
+/**
+ * @file crypto_cipher.h
+ * @brief 加密算法库适配
+ * Licensed under the MIT licenses.
+ *
+ * @version 1.0
+ * @author OWenT
+ * @date 2017.07.25
+ *
+ * @see https://en.wikipedia.org/wiki/XXTEA
+ * @history
+ *
+ *
+ */
+
+#ifndef _UTIL_ALGORITHM_CRYPTO_CIPHER_H_
+#define _UTIL_ALGORITHM_CRYPTO_CIPHER_H_
+
+#pragma once
+
+// CRYPTO_USE_OPENSSL, CRYPTO_USE_LIBRESSL,CRYPTO_USE_BORINGSSL, CRYPTO_USE_MBEDTLS
+
+#if defined(CRYPTO_USE_OPENSSL) || defined(CRYPTO_USE_LIBRESSL) || defined(CRYPTO_USE_BORINGSSL)
+
+#include <openssl/evp.h>
+#include <openssl/md5.h>
+#include <openssl/sha.h>
+
+#define CRYPTO_ENABLED 1
+
+#elif defined(CRYPTO_USE_MBEDTLS)
+
+#include <mbedtls/cipher.h>
+#include <mbedtls/md.h>
+
+#define CRYPTO_ENABLED 1
+
+#endif
+
+#ifdef CRYPTO_ENABLED
+
+#include <string>
+#include <vector>
+
+#include "xxtea.h"
+
+namespace util {
+    namespace crypto {
+        class cipher {
+            struct method_t {
+                enum type {
+                    EN_CMT_INVALID = 0, // inner
+                    EN_CMT_XXTEA = 1,   // inner
+                    EN_CMT_CIPHER
+                };
+            };
+
+            struct mode_t {
+                enum type { EN_CMODE_ENCRYPT = 0x01, EN_CMODE_DECRYPT = 0x02 };
+            };
+
+#if defined(CRYPTO_USE_OPENSSL) || defined(CRYPTO_USE_LIBRESSL) || defined(CRYPTO_USE_BORINGSSL)
+            typedef EVP_CIPHER cipher_kt_t;
+            typedef EVP_CIPHER_CTX cipher_evp_t;
+            typedef EVP_MD digest_type_t;
+            enum {
+                MAX_KEY_LENGTH = EVP_MAX_KEY_LENGTH, //
+                MAX_IV_LENGTH = EVP_MAX_IV_LENGTH,   //
+                MAX_MD_SIZE = EVP_MAX_MD_SIZE
+            };
+
+#elif defined(CRYPTO_USE_MBEDTLS)
+            typedef mbedtls_cipher_info_t cipher_kt_t;
+            typedef mbedtls_cipher_context_t cipher_evp_t;
+            typedef mbedtls_md_info_t digest_type_t;
+            enum {
+                MAX_KEY_LENGTH = 64, //
+                MAX_IV_LENGTH = MBEDTLS_MAX_IV_LENGTH,
+                MAX_MD_SIZE = MBEDTLS_MD_MAX_SIZE
+            };
+#endif
+
+        public:
+            cipher();
+            ~cipher();
+
+            bool init(const char *name, int mode = mode_t::EN_CMODE_ENCRYPT | mode_t::EN_CMODE_DECRYPT);
+            bool close();
+
+            uint32_t get_iv_size() const;
+            uint32_t get_key_bits() const;
+            uint32_t get_block_size() const;
+
+            /**
+             * @biref               encrypt data
+             * @param input         buffer holding the input data
+             * @param ilen          length of the input data
+             * @param output        buffer for the output data. Should be able to hold at
+             *                      least ilen + block_size. Cannot be the same buffer as
+             *                      input!
+             * @param olen          length of the output data, will be filled with the
+             *                      actual number of bytes written.
+             */
+            int encrypt(const unsigned char *input, size_t ilen, unsigned char *output, size_t *olen);
+
+            /**
+             * @biref               decrypt data
+             * @param input         buffer holding the input data
+             * @param ilen          length of the input data
+             * @param output        buffer for the output data. Should be able to hold at
+             *                      least ilen + block_size. Cannot be the same buffer as
+             *                      input!
+             * @param olen          length of the output data, will be filled with the
+             *                      actual number of bytes written.
+             */
+            int decrypt(const unsigned char *input, size_t ilen, unsigned char *output, size_t *olen);
+
+        public:
+            static const cipher_kt_t *get_cipher_by_name(const char *name);
+            static void split_ciphers(const std::string &in, std::vector<std::string> &out);
+
+        private:
+            method_t::type method_;
+            const cipher_kt_t *cipher_kt_;
+            typedef struct { ::util::xxtea_key key; } xxtea_context_t;
+            typedef struct {
+                cipher_evp_t *enc; // used for encrypt
+                cipher_evp_t *dec; // used for decrypt
+            } cipher_context_t;
+            union {
+                cipher_context_t cipher_context_;
+                xxtea_context_t xxtea_context_;
+            };
+            std::vector<unsigned char> iv_;
+        };
+    }
+}
+
+#endif
+
+#endif
---
+++ src/algorithm/crypto_cipher.cpp
@@ -0,0 +1,405 @@
+
+#include <common/string_oprs.h>
+
+#include <algorithm/crypto_cipher.h>
+
+#ifdef CRYPTO_ENABLED
+
+namespace util {
+    namespace crypto {
+        namespace details {
+            static const char *supported_ciphers[] = {
+                "xxtea",
+                "rc4",
+                "aes-128-cfb",
+                "aes-192-cfb",
+                "aes-256-cfb",
+                "aes-128-ctr",
+                "aes-192-ctr",
+                "aes-256-ctr",
+                "bf-cfb",
+                "camellia-128-cfb",
+                "camellia-192-cfb",
+                "camellia-256-cfb",
+                "chacha20",         // only available on openssl 1.1.0 and upper
+                "chacha20-poly1305" // only available on openssl 1.1.0 and upper
+            };
+
+#if defined(CRYPTO_USE_MBEDTLS)
+            static const char *supported_ciphers_mbedtls[] = {
+                "xxtea",
+                "ARC4-128",
+                "AES-128-CFB128",
+                "AES-192-CFB128",
+                "AES-256-CFB128",
+                "AES-128-CTR",
+                "AES-192-CTR",
+                "AES-256-CTR",
+                "BLOWFISH-CFB64",
+                "CAMELLIA-128-CFB128",
+                "CAMELLIA-192-CFB128",
+                "CAMELLIA-256-CFB128",
+                "CHACHA20",         // only available on later mbedtls version, @see https://github.com/ARMmbed/mbedtls/pull/485
+                "CHACHA20-POLY1305" // only available on later mbedtls version, @see https://github.com/ARMmbed/mbedtls/pull/485
+            };
+#endif
+        }
+
+        cipher::cipher() : method_(method_t::EN_CMT_INVALID), cipher_kt_(NULL) {}
+        cipher::~cipher() { close(); }
+
+        bool cipher::init(const char *name, int mode) {
+            if (method_ != method_t::EN_CMT_INVALID) {
+                return false;
+            }
+
+            if (NULL == name) {
+                return false;
+            }
+
+            if (0 == UTIL_STRFUNC_STRCASE_CMP(name, "xxtea")) {
+                method_ = method_t::EN_CMT_XXTEA;
+                return true;
+            }
+
+            cipher_kt_ = get_cipher_by_name(name);
+            if (NULL == cipher_kt_) {
+                return false;
+            }
+
+            bool ret = true;
+#if defined(CRYPTO_USE_OPENSSL) || defined(CRYPTO_USE_LIBRESSL) || defined(CRYPTO_USE_BORINGSSL)
+            do {
+                if (mode & mode_t::EN_CMODE_ENCRYPT) {
+                    cipher_context_.enc = EVP_CIPHER_CTX_new();
+
+                    if (NULL == cipher_context_.enc) {
+                        ret = false;
+                        break;
+                    }
+                    if (!EVP_CipherInit_ex(cipher_context_.enc, cipher_kt_, NULL, NULL, NULL, 1)) {
+                        ret = false;
+                        break;
+                    }
+                } else {
+                    cipher_context_.enc = NULL;
+                }
+
+                if (mode & mode_t::EN_CMODE_DECRYPT) {
+                    cipher_context_.dec = EVP_CIPHER_CTX_new();
+
+                    if (NULL == cipher_context_.dec) {
+                        ret = false;
+                        break;
+                    }
+
+                    if (!EVP_CipherInit_ex(cipher_context_.dec, cipher_kt_, NULL, NULL, NULL, 0)) {
+                        ret = false;
+                        break;
+                    }
+                } else {
+                    cipher_context_.dec = NULL;
+                }
+
+            } while (false);
+
+            if (false == ret) {
+                if ((mode & mode_t::EN_CMODE_ENCRYPT) && NULL != cipher_context_.enc) {
+                    EVP_CIPHER_CTX_free(cipher_context_.enc);
+                    cipher_context_.enc = NULL;
+                }
+
+                if ((mode & mode_t::EN_CMODE_DECRYPT) && NULL != cipher_context_.dec) {
+                    EVP_CIPHER_CTX_free(cipher_context_.dec);
+                    cipher_context_.dec = NULL;
+                }
+            }
+
+#elif defined(CRYPTO_USE_MBEDTLS)
+            do {
+                if (mode & mode_t::EN_CMODE_ENCRYPT) {
+                    cipher_context_.enc = (cipher_evp_t *)malloc(sizeof(cipher_evp_t));
+
+                    if (NULL == cipher_context_.enc) {
+                        ret = false;
+                        break;
+                    }
+
+                    memset(cipher_context_.enc, 0, sizeof(cipher_evp_t));
+                    mbedtls_cipher_init(cipher_context_.enc);
+                    if (mbedtls_cipher_setup(cipher_context_.enc, cipher_kt_) != 0) {
+                        ret = false;
+                        break;
+                    }
+                } else {
+                    cipher_context_.enc = NULL;
+                }
+
+                if (mode & mode_t::EN_CMODE_DECRYPT) {
+                    cipher_context_.dec = (cipher_evp_t *)malloc(sizeof(cipher_evp_t));
+
+                    if (NULL == cipher_context_.dec) {
+                        ret = false;
+                        break;
+                    }
+
+                    memset(cipher_context_.dec, 0, sizeof(cipher_evp_t));
+                    mbedtls_cipher_init(cipher_context_.dec);
+                    if (mbedtls_cipher_setup(cipher_context_.dec, cipher_kt_) != 0) {
+                        ret = false;
+                        break;
+                    }
+                } else {
+                    cipher_context_.dec = NULL;
+                }
+
+            } while (false);
+
+            if (false == ret) {
+                if ((mode & mode_t::EN_CMODE_ENCRYPT) && NULL != cipher_context_.enc) {
+                    mbedtls_cipher_free(cipher_context_.enc);
+                    free(cipher_context_.enc);
+                    cipher_context_.enc = NULL;
+                }
+
+                if ((mode & mode_t::EN_CMODE_DECRYPT) && NULL != cipher_context_.dec) {
+                    mbedtls_cipher_free(cipher_context_.dec);
+                    free(cipher_context_.dec);
+                    cipher_context_.dec = NULL;
+                }
+            }
+#endif
+            if (ret) {
+                method_ = method_t::EN_CMT_CIPHER;
+            }
+            return ret;
+        }
+
+        bool cipher::close() {
+
+            do {
+                if (method_ == method_t::EN_CMT_INVALID) {
+                    return false;
+                } else if (method_ == method_t::EN_CMT_XXTEA) {
+                    // just do nothing when using xxtea
+                    break;
+                }
+
+// cipher cleanup
+#if defined(CRYPTO_USE_OPENSSL) || defined(CRYPTO_USE_LIBRESSL) || defined(CRYPTO_USE_BORINGSSL)
+                if (NULL != cipher_context_.enc) {
+                    EVP_CIPHER_CTX_free(cipher_context_.enc);
+                    cipher_context_.enc = NULL;
+                }
+
+                if (NULL != cipher_context_.dec) {
+                    EVP_CIPHER_CTX_free(cipher_context_.dec);
+                    cipher_context_.dec = NULL;
+                }
+
+#elif defined(CRYPTO_USE_MBEDTLS)
+                if (NULL != cipher_context_.enc) {
+                    mbedtls_cipher_free(cipher_context_.enc);
+                    free(cipher_context_.enc);
+                    cipher_context_.enc = NULL;
+                }
+
+                if (NULL != cipher_context_.dec) {
+                    mbedtls_cipher_free(cipher_context_.dec);
+                    free(cipher_context_.dec);
+                    cipher_context_.dec = NULL;
+                }
+#endif
+            } while (false);
+
+            method_ = method_t::EN_CMT_INVALID;
+            return true;
+        }
+
+        uint32_t cipher::get_iv_size() const {
+            switch (method_) {
+            case method_t::EN_CMT_INVALID:
+            case method_t::EN_CMT_XXTEA:
+                return 0;
+            case method_t::EN_CMT_CIPHER:
+                if (NULL != cipher_context_.enc) {
+#if defined(CRYPTO_USE_OPENSSL) || defined(CRYPTO_USE_LIBRESSL) || defined(CRYPTO_USE_BORINGSSL)
+                    return static_cast<uint32_t> EVP_CIPHER_CTX_iv_length(cipher_context_.enc);
+#elif defined(CRYPTO_USE_MBEDTLS)
+                    return static_cast<uint32_t> mbedtls_cipher_get_iv_size(cipher_context_.enc);
+#endif
+                } else if (NULL != cipher_context_.dec) {
+#if defined(CRYPTO_USE_OPENSSL) || defined(CRYPTO_USE_LIBRESSL) || defined(CRYPTO_USE_BORINGSSL)
+                    return static_cast<uint32_t> EVP_CIPHER_CTX_iv_length(cipher_context_.dec);
+#elif defined(CRYPTO_USE_MBEDTLS)
+                    return static_cast<uint32_t> mbedtls_cipher_get_iv_size(cipher_context_.dec);
+#endif
+                } else {
+                    return 0;
+                }
+            default:
+                return 0;
+            }
+        }
+
+        uint32_t cipher::get_key_bits() const {
+            switch (method_) {
+            case method_t::EN_CMT_INVALID:
+                return 0;
+            case method_t::EN_CMT_XXTEA:
+                return sizeof(::util::xxtea_key) * 8;
+            case method_t::EN_CMT_CIPHER:
+                if (NULL != cipher_context_.enc) {
+#if defined(CRYPTO_USE_OPENSSL) || defined(CRYPTO_USE_LIBRESSL) || defined(CRYPTO_USE_BORINGSSL)
+                    return static_cast<uint32_t> EVP_CIPHER_CTX_key_length(cipher_context_.enc) * 8;
+#elif defined(CRYPTO_USE_MBEDTLS)
+                    return static_cast<uint32_t> mbedtls_cipher_get_key_bitlen(cipher_context_.enc);
+#endif
+                } else if (NULL != cipher_context_.dec) {
+#if defined(CRYPTO_USE_OPENSSL) || defined(CRYPTO_USE_LIBRESSL) || defined(CRYPTO_USE_BORINGSSL)
+                    return static_cast<uint32_t> EVP_CIPHER_CTX_key_length(cipher_context_.dec) * 8;
+#elif defined(CRYPTO_USE_MBEDTLS)
+                    return static_cast<uint32_t> mbedtls_cipher_get_key_bitlen(cipher_context_.dec);
+#endif
+                } else {
+                    return 0;
+                }
+            default:
+                return 0;
+            }
+        }
+
+        uint32_t cipher::get_block_size() const {
+            switch (method_) {
+            case method_t::EN_CMT_INVALID:
+                return 0;
+            case method_t::EN_CMT_XXTEA:
+                return 0x04;
+            case method_t::EN_CMT_CIPHER:
+                if (NULL != cipher_context_.enc) {
+#if defined(CRYPTO_USE_OPENSSL) || defined(CRYPTO_USE_LIBRESSL) || defined(CRYPTO_USE_BORINGSSL)
+                    return static_cast<uint32_t> EVP_CIPHER_CTX_block_size(cipher_context_.enc);
+#elif defined(CRYPTO_USE_MBEDTLS)
+                    return static_cast<uint32_t> mbedtls_cipher_get_block_size(cipher_context_.enc);
+#endif
+                } else if (NULL != cipher_context_.dec) {
+#if defined(CRYPTO_USE_OPENSSL) || defined(CRYPTO_USE_LIBRESSL) || defined(CRYPTO_USE_BORINGSSL)
+                    return static_cast<uint32_t> EVP_CIPHER_CTX_block_size(cipher_context_.dec);
+#elif defined(CRYPTO_USE_MBEDTLS)
+                    return static_cast<uint32_t> mbedtls_cipher_get_block_size(cipher_context_.dec);
+#endif
+                } else {
+                    return 0;
+                }
+            default:
+                return 0;
+            }
+        }
+
+        int cipher::encrypt(const unsigned char *input, size_t ilen, unsigned char *output, size_t *olen) {
+            if (input == NULL || ilen <= 0 || output == NULL || olen <= 0 || *olen < ilen + get_block_size()) {
+                return -1;
+            }
+
+            switch (method_) {
+            case method_t::EN_CMT_INVALID:
+                return -2;
+            case method_t::EN_CMT_XXTEA: {
+                memcpy(output, input, ilen);
+                *olen = ilen;
+                if (ilen & 0x03) {
+                    memset(output + ilen, 0x04 - (ilen & 0x03));
+
+                    ilen = (ilen | 0x03) + 1;
+                }
+                util::xxtea_encrypt(&xxtea_context_.key, output, ilen);
+                return 0;
+            }
+            case method_t::EN_CMT_CIPHER:
+                if (NULL == cipher_context_.enc) {
+                    return -3;
+                }
+#if defined(CRYPTO_USE_OPENSSL) || defined(CRYPTO_USE_LIBRESSL) || defined(CRYPTO_USE_BORINGSSL)
+#elif defined(CRYPTO_USE_MBEDTLS)
+#endif
+            default:
+                return -2;
+            }
+        }
+
+        int cipher::decrypt(const unsigned char *input, size_t ilen, unsigned char *output, size_t *olen) {
+            if (input == NULL || ilen <= 0 || output == NULL || olen <= 0 || *olen < ilen + get_block_size()) {
+                return -1;
+            }
+
+            switch (method_) {
+            case method_t::EN_CMT_INVALID:
+                return -2;
+            case method_t::EN_CMT_XXTEA: {
+                memcpy(output, input, ilen);
+                *olen = ilen;
+                if (ilen & 0x03) {
+                    memset(output + ilen, 0x04 - (ilen & 0x03));
+
+                    ilen = (ilen | 0x03) + 1;
+                }
+                util::xxtea_decrypt(&xxtea_context_.key, output, ilen);
+                return 0;
+            }
+            case method_t::EN_CMT_CIPHER:
+                if (NULL == cipher_context_.enc) {
+                    return -3;
+                }
+#if defined(CRYPTO_USE_OPENSSL) || defined(CRYPTO_USE_LIBRESSL) || defined(CRYPTO_USE_BORINGSSL)
+#elif defined(CRYPTO_USE_MBEDTLS)
+#endif
+            default:
+                return -2;
+            }
+        }
+
+        const cipher::cipher_kt_t *cipher::get_cipher_by_name(const char *name) {
+            size_t sz = sizeof(details::supported_ciphers) / sizeof(const char *);
+            size_t index = 0;
+            for (; index < sz; ++index) {
+                if (0 == UTIL_STRFUNC_STRCASE_CMP(name, details::supported_ciphers[index])) {
+                    break;
+                }
+            }
+
+            if (index >= sz) {
+                return NULL;
+            }
+
+#if defined(CRYPTO_USE_OPENSSL) || defined(CRYPTO_USE_LIBRESSL) || defined(CRYPTO_USE_BORINGSSL)
+            return EVP_get_cipherbyname(details::supported_ciphers[index]);
+#elif defined(CRYPTO_USE_MBEDTLS)
+            return mbedtls_cipher_info_from_string(details::supported_ciphers_mbedtls[index]);
+#endif
+        }
+
+        void cipher::split_ciphers(const std::string &in, std::vector<std::string> &out) {
+            size_t b = 0, e;
+
+            while (b < in.size()) {
+                // skip \r\n\t and space
+                if (in[b] == ' ' || in[b] == '\t' || in[b] == '\r' || in[b] == '\n' || in[b] == ';') {
+                    ++b;
+                    continue;
+                }
+
+                for (e = b + 1; e < in.size(); ++e) {
+                    char c = in[e];
+                    if (c == ' ' || c == '\t' || c == '\r' || c == '\n' || c == ';') {
+                        break;
+                    }
+                }
+
+                out.push_back(in.substr(b, e - b));
+                b = e;
+            }
+        }
+    }
+}
+
+#endif
