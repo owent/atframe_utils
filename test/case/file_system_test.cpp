cdaee341e8870020aaf5f1de294f3a062e67a0dd
owent <admin@owent.net>
2018-01-29 23:47:46 +0800

    增加一些文件系统模块的单元测试

--- test/case/file_system_test.cpp
+++ test/case/file_system_test.cpp
@@ -34,3 +34,44 @@ CASE_TEST(file_system, get_cwd) {
     std::string dir;
     CASE_MSG_INFO() << "Working dir: " << util::file_system::get_cwd() << std::endl;
 }
+
+
+CASE_TEST(file_system, get_file_content) {
+    std::string content;
+#if defined(__unix__)
+    const char *virtual_file = "/proc/cpuinfo";
+    CASE_EXPECT_TRUE(util::file_system::get_file_content(content, virtual_file));
+    CASE_MSG_INFO() << "Cpu Info: " << std::endl << content << std::endl;
+#endif
+
+    CASE_EXPECT_TRUE(util::file_system::get_file_content(content, __FILE__));
+}
+
+CASE_TEST(file_system, mkdir_remove) {
+    if (!util::file_system::is_exist("test-mkdir/relpath")) {
+        CASE_EXPECT_TRUE(util::file_system::mkdir("test-mkdir/relpath", true));
+        CASE_EXPECT_TRUE(util::file_system::is_exist("test-mkdir/relpath"));
+    }
+
+    // remove
+    CASE_EXPECT_TRUE(util::file_system::remove("test-mkdir/relpath"));
+    CASE_EXPECT_FALSE(util::file_system::is_exist("test-mkdir/relpath"));
+
+    std::string dirpath = util::file_system::get_cwd() + "/test-mkdir/abspath";
+    if (!util::file_system::is_exist(dirpath.c_str())) {
+        CASE_EXPECT_TRUE(util::file_system::mkdir(dirpath.c_str(), true));
+        CASE_EXPECT_TRUE(util::file_system::is_exist(dirpath.c_str()));
+    }
+
+    // remove & rename
+    CASE_EXPECT_TRUE(util::file_system::rename(dirpath.c_str(), "test-mkdir/relpath"));
+    CASE_EXPECT_TRUE(util::file_system::is_exist("test-mkdir/relpath"));
+    CASE_EXPECT_FALSE(util::file_system::is_exist(dirpath.c_str()));
+    CASE_EXPECT_TRUE(util::file_system::remove("test-mkdir/relpath"));
+}
+
+CASE_TEST(file_system, file_size) {
+    size_t sz = 0;
+    CASE_EXPECT_TRUE(util::file_system::file_size(__FILE__, sz));
+    CASE_EXPECT_GT(sz, 0);
+}
