b094584008c4979093d470568eb3fcaba164898c
owent <admin@owent.net>
2018-07-25 20:54:09 +0800

    add jemalloc library and library with pic

--- project/cmake/modules/FindJemalloc.cmake
+++ project/cmake/modules/FindJemalloc.cmake
@@ -15,10 +15,12 @@
 #
 # ::
 #
-#   Jemalloc_INCLUDE_DIRS  - where to find jemalloc/jemalloc.h, etc.
-#   Jemalloc_EXECUTABLE    - full path of pproc.
-#   Jemalloc_LIBRARY_DIRS  - directory of libraries of jemalloc.
-#   Jemalloc_FOUND         - True if jemalloc found.
+#   Jemalloc_INCLUDE_DIRS   - where to find jemalloc/jemalloc.h, etc.
+#   Jemalloc_EXECUTABLE     - full path of pproc.
+#   Jemalloc_LIBRARY_DIRS   - directory of libraries of jemalloc.
+#   Jemalloc_LIBRARYIES     - libraries of jemalloc.
+#   Jemalloc_LIBRARYIES_PIC - libraries of jemalloc with pic.
+#   Jemalloc_FOUND          - True if jemalloc found.
 #
 # ::
 #
@@ -31,7 +33,7 @@
 #   (or Jemalloc_ROOT)
 
 #=============================================================================
-# Copyright 2014-2015 OWenT.
+# Copyright 2014-2018 OWenT.
 #
 # Distributed under the OSI-approved BSD License (the "License");
 # see accompanying file Copyright.txt for details.
@@ -57,17 +59,18 @@ endif()
 set(JEMALLOC_NAMES jemalloc jemalloc_pic)
 
 # Try each search configuration.
-find_path(Jemalloc_INCLUDE_DIRS NAMES "jemalloc/jemalloc.h" ${_JEMALLOC_SEARCH_ROOT})
-find_library(Jemalloc_LIBRARY_DIRS NAMES ${JEMALLOC_NAMES} ${_JEMALLOC_SEARCH_ROOT})
-string(REGEX REPLACE "[/\\\\][^/\\\\]*$" "" Jemalloc_LIBRARY_DIRS "${Jemalloc_LIBRARY_DIRS}")
+find_path(Jemalloc_INCLUDE_DIRS NAMES "jemalloc/jemalloc.h" PATH_SUFFIXES include ${_JEMALLOC_SEARCH_ROOT})
+find_library(Jemalloc_LIBRARYIES NAMES ${JEMALLOC_NAMES} PATH_SUFFIXES lib ${_JEMALLOC_SEARCH_ROOT})
+find_library(Jemalloc_LIBRARYIES_PIC NAMES ${JEMALLOC_NAMES} PATH_SUFFIXES lib ${_JEMALLOC_SEARCH_ROOT})
+string(REGEX REPLACE "[/\\\\][^/\\\\]*$" "" Jemalloc_LIBRARY_DIRS ${Jemalloc_LIBRARYIES})
 
-mark_as_advanced(Jemalloc_LIBRARY_DIRS Jemalloc_INCLUDE_DIRS )
+mark_as_advanced(Jemalloc_LIBRARYIES Jemalloc_LIBRARYIES_PIC Jemalloc_LIBRARY_DIRS Jemalloc_INCLUDE_DIRS )
 
 # handle the QUIETLY and REQUIRED arguments and set Jemalloc_FOUND to TRUE if
 # all listed variables are TRUE
 include("FindPackageHandleStandardArgs")
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Jemalloc
-  REQUIRED_VARS Jemalloc_INCLUDE_DIRS Jemalloc_LIBRARY_DIRS
+  REQUIRED_VARS Jemalloc_INCLUDE_DIRS Jemalloc_LIBRARYIES Jemalloc_LIBRARYIES_PIC Jemalloc_LIBRARY_DIRS
   FOUND_VAR Jemalloc_FOUND
 )
 
