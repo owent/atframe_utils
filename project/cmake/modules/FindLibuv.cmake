daaaf75377c3f4dc8ae7ff14230673b58da1621a
owent <admin@owent.net>
2020-03-21 23:20:45 +0800

    修订 FindLibuv.cmake 用以适配libuv自带的CMakeLists.txt

--- project/cmake/modules/FindLibuv.cmake
+++ project/cmake/modules/FindLibuv.cmake
@@ -50,11 +50,33 @@ if (Libuv_ROOT)
 endif()
 
 if(LIBUV_ROOT)
-  set(_LIBUV_SEARCH_ROOT_INC PATHS ${LIBUV_ROOT} ${LIBUV_ROOT}/include NO_DEFAULT_PATH)
+  set(_LIBUV_SEARCH_ROOT_INC PATHS ${LIBUV_ROOT} "${LIBUV_ROOT}/include" NO_DEFAULT_PATH)
   if(CMAKE_SIZEOF_VOID_P MATCHES 8)
-    set(_LIBUV_SEARCH_ROOT_LIB PATHS ${LIBUV_ROOT} ${LIBUV_ROOT}/lib64 ${LIBUV_ROOT}/lib NO_DEFAULT_PATH)
+    if(MSVC AND CMAKE_BUILD_TYPE)
+      set(_LIBUV_SEARCH_ROOT_LIB PATHS ${LIBUV_ROOT} 
+        "${LIBUV_ROOT}/lib64/${CMAKE_BUILD_TYPE}" 
+        "${LIBUV_ROOT}/lib/${CMAKE_BUILD_TYPE}" 
+        "${LIBUV_ROOT}/lib64" 
+        "${LIBUV_ROOT}/lib" NO_DEFAULT_PATH
+      )
+    else()
+      set(_LIBUV_SEARCH_ROOT_LIB PATHS ${LIBUV_ROOT} 
+        "${LIBUV_ROOT}/lib64" 
+        "${LIBUV_ROOT}/lib" NO_DEFAULT_PATH
+      )
+    endif()
   else()
-    set(_LIBUV_SEARCH_ROOT_LIB PATHS ${LIBUV_ROOT} ${LIBUV_ROOT}/lib NO_DEFAULT_PATH)
+    if(MSVC AND CMAKE_BUILD_TYPE)
+      set(_LIBUV_SEARCH_ROOT_LIB PATHS ${LIBUV_ROOT} 
+        "${LIBUV_ROOT}/lib/${CMAKE_BUILD_TYPE}" 
+        "${LIBUV_ROOT}/lib64" 
+        "${LIBUV_ROOT}/lib" NO_DEFAULT_PATH
+      )
+    else()
+      set(_LIBUV_SEARCH_ROOT_LIB PATHS ${LIBUV_ROOT} 
+        "${LIBUV_ROOT}/lib" NO_DEFAULT_PATH
+      )
+    endif()
   endif()
 endif()
 
@@ -76,5 +98,25 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(Libuv
 )
 
 if(Libuv_FOUND)
-    set(LIBUV_FOUND ${Libuv_FOUND})
+  set(LIBUV_FOUND ${Libuv_FOUND})
+
+  if (NOT TARGET libuv)
+    add_library(libuv STATIC IMPORTED)
+    set_target_properties(libuv PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES ${Libuv_INCLUDE_DIRS}
+    )
+    set_target_properties(libuv PROPERTIES
+      IMPORTED_LINK_INTERFACE_LANGUAGES "C;CXX;RC"
+      IMPORTED_LOCATION ${Libuv_LIBRARIES}
+    )
+
+    if (WIN32)
+      set_target_properties(libuv PROPERTIES
+        INTERFACE_LINK_LIBRARIES "psapi;iphlpapi;userenv;ws2_32"
+      )
+    endif ()
+  endif ()
+else ()
+  unset(Libuv_INCLUDE_DIRS CACHE)
+  unset(Libuv_LIBRARIES CACHE)
 endif()
