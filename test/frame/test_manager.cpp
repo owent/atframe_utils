0009438d5c45145d5b5720c78075c91d9815ad54
owent <admin@owent.net>
2020-03-30 21:08:43 +0800

    fix test_manager

--- test/frame/test_manager.cpp
+++ test/frame/test_manager.cpp
@@ -9,19 +9,22 @@
 
 #include <cstring>
 #include <iostream>
+#include <list>
 #include <sstream>
 #include <string>
 #include <vector>
-#include <list>
+
+
+#include <std/smart_ptr.h>
+#include <std/thread.h>
+
+#include <config/atframe_utils_build_feature.h>
+#include <config/compile_optimize.h>
+#include <config/compiler_features.h>
 
 #if defined(UTIL_CONFIG_COMPILER_CXX_STATIC_ASSERT) && UTIL_CONFIG_COMPILER_CXX_STATIC_ASSERT
 #include <type_traits>
 #endif
-
-#include <std/smart_ptr.h>
-
-#include <config/compiler_features.h>
-#include <config/compile_optimize.h>
 
 #include "cli/cmd_option.h"
 #include "cli/cmd_option_phoenix.h"
@@ -37,18 +40,18 @@ namespace detail {
         int *failed_counter_ptr;
     };
 #if defined(UTIL_CONFIG_COMPILER_CXX_STATIC_ASSERT) && UTIL_CONFIG_COMPILER_CXX_STATIC_ASSERT
-    #if (defined(__cplusplus) && __cplusplus >= 201402L) || ((defined(_MSVC_LANG) && _MSVC_LANG >= 201402L))
-        UTIL_CONFIG_STATIC_ASSERT(std::is_trivially_copyable<test_manager_tls_block_t>::value);
-    #elif (defined(__cplusplus) && __cplusplus >= 201103L) || ((defined(_MSVC_LANG) && _MSVC_LANG >= 201103L))
-        UTIL_CONFIG_STATIC_ASSERT(std::is_trivial<test_manager_tls_block_t>::value);
-    #else
-        UTIL_CONFIG_STATIC_ASSERT(std::is_pod<test_manager_tls_block_t>::value);
-    #endif
+#if (defined(__cplusplus) && __cplusplus >= 201402L) || ((defined(_MSVC_LANG) && _MSVC_LANG >= 201402L))
+    UTIL_CONFIG_STATIC_ASSERT(std::is_trivially_copyable<test_manager_tls_block_t>::value);
+#elif (defined(__cplusplus) && __cplusplus >= 201103L) || ((defined(_MSVC_LANG) && _MSVC_LANG >= 201103L))
+    UTIL_CONFIG_STATIC_ASSERT(std::is_trivial<test_manager_tls_block_t>::value);
+#else
+    UTIL_CONFIG_STATIC_ASSERT(std::is_pod<test_manager_tls_block_t>::value);
+#endif
 #endif
 
-    test_manager_tls_block_t* get_test_manager_tls_block() {
+    test_manager_tls_block_t *get_test_manager_tls_block() {
         static THREAD_TLS unsigned char ret[sizeof(test_manager_tls_block_t)] = {0};
-        return reinterpret_cast<test_manager_tls_block_t*>(ret);
+        return reinterpret_cast<test_manager_tls_block_t *>(ret);
     }
 #else
 
@@ -57,7 +60,7 @@ namespace detail {
         int *success_counter_ptr;
         int *failed_counter_ptr;
 
-        test_manager_tls_block_t(): success_counter_ptr(NULL), failed_counter_ptr(NULL) {}
+        test_manager_tls_block_t() : success_counter_ptr(NULL), failed_counter_ptr(NULL) {}
     };
     static pthread_once_t gt_test_manager_tls_block_once = PTHREAD_ONCE_INIT;
     static pthread_key_t  gt_test_manager_tls_block_key;
@@ -69,9 +72,11 @@ namespace detail {
         }
     }
 
-    static void init_pthread_test_manager_tls_block() { (void)pthread_key_create(&gt_test_manager_tls_block_key, dtor_pthread_test_manager_tls_block); }
+    static void init_pthread_test_manager_tls_block() {
+        (void)pthread_key_create(&gt_test_manager_tls_block_key, dtor_pthread_test_manager_tls_block);
+    }
 
-    test_manager_tls_block_t* get_test_manager_tls_block() {
+    test_manager_tls_block_t *get_test_manager_tls_block() {
         (void)pthread_once(&gt_test_manager_tls_block_once, init_pthread_test_manager_tls_block);
         test_manager_tls_block_t *block = reinterpret_cast<test_manager_tls_block_t *>(pthread_getspecific(gt_test_manager_tls_block_key));
         if (NULL == block) {
@@ -82,28 +87,28 @@ namespace detail {
     }
 
     struct gt_test_manager_tls_block_main_thread_dtor_t {
-        test_manager_tls_block_t* block_ptr;
-        gt_test_manager_tls_block_main_thread_dtor_t(){
+        test_manager_tls_block_t *block_ptr;
+        gt_test_manager_tls_block_main_thread_dtor_t() {
             block_ptr = get_test_manager_tls_block();
             if (NULL != block_ptr) {
                 block_ptr->success_counter_ptr = NULL;
-                block_ptr->failed_counter_ptr = NULL;
+                block_ptr->failed_counter_ptr  = NULL;
             }
         }
 
         ~gt_test_manager_tls_block_main_thread_dtor_t() {
             pthread_setspecific(gt_test_manager_tls_block_key, NULL);
-            dtor_pthread_test_manager_tls_block(reinterpret_cast<void*>(block_ptr));
+            dtor_pthread_test_manager_tls_block(reinterpret_cast<void *>(block_ptr));
         }
     };
     static gt_test_manager_tls_block_main_thread_dtor_t gt_test_manager_tls_block_main_thread_dtor;
 #endif
     struct topological_sort_object_t {
         std::string name;
-        void* object;
+        void *      object;
 
-        size_t dependency_count;
-        std::list<topological_sort_object_t*> depend_by;
+        size_t                                 dependency_count;
+        std::list<topological_sort_object_t *> depend_by;
     };
 } // namespace detail
 
@@ -131,8 +136,8 @@ bool test_manager::pick_param_str_t::operator<(const pick_param_str_t &other) co
 #endif
 
 test_manager::test_manager() {
-    success_                                 = 0;
-    failed_                                  = 0;
+    success_ = 0;
+    failed_  = 0;
 }
 
 test_manager::~test_manager() {}
@@ -176,23 +181,22 @@ int test_manager::run() {
 #else
 
 
-static void topological_sort(UTIL_UNIT_TEST_MACRO_AUTO_MAP(std::string, detail::topological_sort_object_t)& in, 
-    std::vector<detail::topological_sort_object_t*>& out) {
+static void topological_sort(UTIL_UNIT_TEST_MACRO_AUTO_MAP(std::string, detail::topological_sort_object_t) & in,
+                             std::vector<detail::topological_sort_object_t *> &out) {
     typedef UTIL_UNIT_TEST_MACRO_AUTO_MAP(std::string, detail::topological_sort_object_t) index_by_name_t;
     out.reserve(in.size());
 
-    for (index_by_name_t::iterator iter = in.begin(); iter != in.end(); ++ iter) {
+    for (index_by_name_t::iterator iter = in.begin(); iter != in.end(); ++iter) {
         if (0 == iter->second.dependency_count) {
             out.push_back(&iter->second);
         }
     }
 
-    for (size_t i = 0; i < out.size(); ++ i) {
-        for(std::list<detail::topological_sort_object_t*>::iterator iter = out[i]->depend_by.begin();
-            iter != out[i]->depend_by.end(); ++ iter
-        ) {
+    for (size_t i = 0; i < out.size(); ++i) {
+        for (std::list<detail::topological_sort_object_t *>::iterator iter = out[i]->depend_by.begin(); iter != out[i]->depend_by.end();
+             ++iter) {
             if ((*iter)->dependency_count > 0) {
-                -- (*iter)->dependency_count;
+                --(*iter)->dependency_count;
 
                 if (0 == (*iter)->dependency_count) {
                     out.push_back(*iter);
@@ -206,41 +210,40 @@ int test_manager::run_event_on_start() {
     // generate topological_sort_object_t
     typedef UTIL_UNIT_TEST_MACRO_AUTO_MAP(std::string, detail::topological_sort_object_t) index_by_name_t;
     index_by_name_t index_by_name;
-    for (size_t i = 0; i < evt_on_starts_.size(); ++ i) {
-        detail::topological_sort_object_t& obj = index_by_name[evt_on_starts_[i].first];
+    for (size_t i = 0; i < evt_on_starts_.size(); ++i) {
+        detail::topological_sort_object_t &obj = index_by_name[evt_on_starts_[i].first];
         obj.name = evt_on_starts_[i].first;
-        obj.object = reinterpret_cast<void*>(evt_on_starts_[i].second);
+        obj.object = reinterpret_cast<void *>(evt_on_starts_[i].second);
         obj.dependency_count = 0;
     }
 
-    for (size_t i = 0; i < evt_on_starts_.size(); ++ i) {
-        detail::topological_sort_object_t& obj = index_by_name[evt_on_starts_[i].first];
+    for (size_t i = 0; i < evt_on_starts_.size(); ++i) {
+        detail::topological_sort_object_t &obj = index_by_name[evt_on_starts_[i].first];
 
         for (test_on_start_base::after_set_t::iterator iter = evt_on_starts_[i].second->after.begin();
-            iter != evt_on_starts_[i].second->after.end(); ++ iter
-        ) {
+             iter != evt_on_starts_[i].second->after.end(); ++iter) {
             index_by_name_t::iterator dep_iter = index_by_name.find(*iter);
             if (dep_iter == index_by_name.end()) {
                 util::cli::shell_stream ss(std::cerr);
-                ss() << util::cli::shell_font_style::SHELL_FONT_COLOR_GREEN << util::cli::shell_font_style::SHELL_FONT_SPEC_BOLD << "[ WARNING  ] "
-                    << util::cli::shell_font_style::SHELL_FONT_SPEC_NULL << "On Start Event " << evt_on_starts_[i].first << " is configured run after "
-                    << (*iter)<< ", but "<< (*iter)<< "not found." << std::endl;
+                ss() << util::cli::shell_font_style::SHELL_FONT_COLOR_GREEN << util::cli::shell_font_style::SHELL_FONT_SPEC_BOLD
+                     << "[ WARNING  ] " << util::cli::shell_font_style::SHELL_FONT_SPEC_NULL << "On Start Event " << evt_on_starts_[i].first
+                     << " is configured run after " << (*iter) << ", but " << (*iter) << "not found." << std::endl;
                 continue;
             }
 
-            ++ obj.dependency_count;
+            ++obj.dependency_count;
             dep_iter->second.depend_by.push_back(&obj);
         }
     }
 
-    std::vector<detail::topological_sort_object_t*> run_order;
+    std::vector<detail::topological_sort_object_t *> run_order;
     topological_sort(index_by_name, run_order);
 
-    for (size_t i = 0; i < run_order.size(); ++ i) {
+    for (size_t i = 0; i < run_order.size(); ++i) {
         util::cli::shell_stream ss(std::cout);
         ss() << util::cli::shell_font_style::SHELL_FONT_COLOR_GREEN << util::cli::shell_font_style::SHELL_FONT_SPEC_BOLD << "[ On Start ] "
-            << util::cli::shell_font_style::SHELL_FONT_SPEC_NULL << "Running " << run_order[i]->name << std::endl;
-            reinterpret_cast<on_start_ptr_type>(run_order[i]->object)->run();
+             << util::cli::shell_font_style::SHELL_FONT_SPEC_NULL << "Running " << run_order[i]->name << std::endl;
+        reinterpret_cast<on_start_ptr_type>(run_order[i]->object)->run();
     }
 
     return 0;
@@ -250,42 +253,41 @@ int test_manager::run_event_on_exit() {
     typedef UTIL_UNIT_TEST_MACRO_AUTO_MAP(std::string, detail::topological_sort_object_t) index_by_name_t;
     // generate topological_sort_object_t
     index_by_name_t index_by_name;
-    for (size_t i = 0; i < evt_on_exits_.size(); ++ i) {
-        detail::topological_sort_object_t& obj = index_by_name[evt_on_exits_[i].first];
+    for (size_t i = 0; i < evt_on_exits_.size(); ++i) {
+        detail::topological_sort_object_t &obj = index_by_name[evt_on_exits_[i].first];
         obj.name = evt_on_exits_[i].first;
-        obj.object = reinterpret_cast<void*>(evt_on_exits_[i].second);
+        obj.object = reinterpret_cast<void *>(evt_on_exits_[i].second);
         obj.dependency_count = 0;
     }
 
-    for (size_t i = 0; i < evt_on_exits_.size(); ++ i) {
-        detail::topological_sort_object_t& obj = index_by_name[evt_on_exits_[i].first];
+    for (size_t i = 0; i < evt_on_exits_.size(); ++i) {
+        detail::topological_sort_object_t &obj = index_by_name[evt_on_exits_[i].first];
 
         for (test_on_exit_base::before_set_t::iterator iter = evt_on_exits_[i].second->before.begin();
-            iter != evt_on_exits_[i].second->before.end(); ++ iter
-        ) {
+             iter != evt_on_exits_[i].second->before.end(); ++iter) {
             index_by_name_t::iterator dep_iter = index_by_name.find(*iter);
             if (dep_iter == index_by_name.end()) {
                 util::cli::shell_stream ss(std::cerr);
-                ss() << util::cli::shell_font_style::SHELL_FONT_COLOR_GREEN << util::cli::shell_font_style::SHELL_FONT_SPEC_BOLD << "[ WARNING  ] "
-                    << util::cli::shell_font_style::SHELL_FONT_SPEC_NULL << "On Exit Event " << evt_on_exits_[i].first << " is configured run before "
-                    << (*iter)<< ", but "<< (*iter)<< "not found." << std::endl;
+                ss() << util::cli::shell_font_style::SHELL_FONT_COLOR_GREEN << util::cli::shell_font_style::SHELL_FONT_SPEC_BOLD
+                     << "[ WARNING  ] " << util::cli::shell_font_style::SHELL_FONT_SPEC_NULL << "On Exit Event " << evt_on_exits_[i].first
+                     << " is configured run before " << (*iter) << ", but " << (*iter) << "not found." << std::endl;
                 continue;
             }
 
-            ++ obj.dependency_count;
+            ++obj.dependency_count;
             dep_iter->second.depend_by.push_back(&obj);
         }
     }
 
-    std::vector<detail::topological_sort_object_t*> run_order;
+    std::vector<detail::topological_sort_object_t *> run_order;
     topological_sort(index_by_name, run_order);
 
-    for (size_t i = 0; i < run_order.size(); ++ i) {
+    for (size_t i = 0; i < run_order.size(); ++i) {
         size_t idx = run_order.size() - 1 - i;
         util::cli::shell_stream ss(std::cout);
         ss() << util::cli::shell_font_style::SHELL_FONT_COLOR_GREEN << util::cli::shell_font_style::SHELL_FONT_SPEC_BOLD << "[ On Exit  ] "
-            << util::cli::shell_font_style::SHELL_FONT_SPEC_NULL << "Running " << run_order[idx]->name << std::endl;
-             reinterpret_cast<on_exit_ptr_type>(run_order[idx]->object)->run();
+             << util::cli::shell_font_style::SHELL_FONT_SPEC_NULL << "Running " << run_order[idx]->name << std::endl;
+        reinterpret_cast<on_exit_ptr_type>(run_order[idx]->object)->run();
     }
 
     return 0;
@@ -293,15 +295,15 @@ int test_manager::run_event_on_exit() {
 
 int test_manager::run() {
     success_ = 0;
-    failed_  = 0;
+    failed_ = 0;
 
-    clock_t                 all_begin_time = clock();
+    clock_t all_begin_time = clock();
     util::cli::shell_stream ss(std::cout);
     ss() << util::cli::shell_font_style::SHELL_FONT_COLOR_GREEN << util::cli::shell_font_style::SHELL_FONT_SPEC_BOLD << "[==========] "
          << util::cli::shell_font_style::SHELL_FONT_SPEC_NULL << "Running " << tests_.size() << " test(s)" << std::endl;
 
     for (test_data_type::iterator iter = tests_.begin(); iter != tests_.end(); ++iter) {
-        bool check_test_group_passed    = run_cases_.empty();
+        bool check_test_group_passed = run_cases_.empty();
         bool check_test_group_has_cases = false;
 
         if (!check_test_group_passed) {
@@ -333,7 +335,7 @@ int test_manager::run() {
                 if (!check_test_case_passed) {
                     std::string full_name;
                     full_name.reserve(iter->first.size() + 1 + iter2->first.size());
-                    full_name              = iter->first + "." + iter2->first;
+                    full_name = iter->first + "." + iter2->first;
                     check_test_case_passed = run_cases_.end() != run_cases_.find(full_name);
                 }
             }
@@ -425,45 +427,41 @@ std::string test_manager::get_expire_time(clock_t begin, clock_t end) {
     return ss.str();
 }
 
-void test_manager::set_counter_ptr(int* success_counter_ptr, int* failed_counter_ptr) {
-    detail::test_manager_tls_block_t* block = detail::get_test_manager_tls_block();
+void test_manager::set_counter_ptr(int *success_counter_ptr, int *failed_counter_ptr) {
+    detail::test_manager_tls_block_t *block = detail::get_test_manager_tls_block();
     if (NULL != block) {
         block->success_counter_ptr = success_counter_ptr;
-        block->failed_counter_ptr = failed_counter_ptr;
+        block->failed_counter_ptr  = failed_counter_ptr;
     }
 }
 
 void test_manager::iinc_success_counter() {
-    detail::test_manager_tls_block_t* block = detail::get_test_manager_tls_block();
+    detail::test_manager_tls_block_t *block = detail::get_test_manager_tls_block();
     if (likely(NULL != block)) {
-        ++ block->success_counter_ptr;
+        ++block->success_counter_ptr;
         return;
     }
 
     util::cli::shell_stream ss(std::cerr);
-    ss() << util::cli::shell_font_style::SHELL_FONT_COLOR_RED << util::cli::shell_font_style::SHELL_FONT_SPEC_BOLD 
-         << "[==========] " << "Expect expression can not be used when not running test case." << std::endl;
+    ss() << util::cli::shell_font_style::SHELL_FONT_COLOR_RED << util::cli::shell_font_style::SHELL_FONT_SPEC_BOLD << "[==========] "
+         << "Expect expression can not be used when not running test case." << std::endl;
 }
 
 void test_manager::inc_failed_counter() {
-    detail::test_manager_tls_block_t* block = detail::get_test_manager_tls_block();
+    detail::test_manager_tls_block_t *block = detail::get_test_manager_tls_block();
     if (likely(NULL != block)) {
-        ++ block->failed_counter_ptr;
+        ++block->failed_counter_ptr;
         return;
     }
 
     util::cli::shell_stream ss(std::cerr);
-    ss() << util::cli::shell_font_style::SHELL_FONT_COLOR_RED << util::cli::shell_font_style::SHELL_FONT_SPEC_BOLD 
-         << "[==========] " << "Expect expression can not be used when not running test case." << std::endl;
+    ss() << util::cli::shell_font_style::SHELL_FONT_COLOR_RED << util::cli::shell_font_style::SHELL_FONT_SPEC_BOLD << "[==========] "
+         << "Expect expression can not be used when not running test case." << std::endl;
 }
 
-int run_event_on_start() {
-    return test_manager::me().run_event_on_start();
-}
+int run_event_on_start() { return test_manager::me().run_event_on_start(); }
 
-int run_event_on_exit() {
-    return test_manager::me().run_event_on_exit();
-}
+int run_event_on_exit() { return test_manager::me().run_event_on_exit(); }
 
 int run_tests(int argc, char *argv[]) {
     std::vector<std::string> run_cases;
