76a41e310466e511aaa6f4837e42cbc4b44110aa
owentou <owentou@tencent.com>
2020-03-30 16:26:34 +0800

    新增单元测试的开始和结束事件

---
+++ test/case/unit_test_event_test.cpp
@@ -0,0 +1,31 @@
+﻿#include "frame/test_macros.h"
+
+static int g_unit_test_event_on_start_status = 0;
+
+CASE_TEST_EVENT_ON_START(unit_test_event_on_start_1) {
+    if (0 == g_unit_test_event_on_start_status) {
+        g_unit_test_event_on_start_status = 1;
+    }
+}
+
+CASE_TEST_EVENT_ON_START(unit_test_event_on_start_2, "unit_test_event_on_start_1") {
+    if (1 == g_unit_test_event_on_start_status) {
+        g_unit_test_event_on_start_status = 2;
+    }
+}
+
+CASE_TEST(unit_test, event_on_start) {
+    CASE_EXPECT_EQ(2, g_unit_test_event_on_start_status);
+}
+
+CASE_TEST_EVENT_ON_EXIT(unit_test_event_on_exit_1) {
+    if (3 == g_unit_test_event_on_start_status) {
+        util::cli::shell_stream(std::cout)()<< "[ FINISH   ] unit_test_event_on_exit_1"<< std::endl;
+    }
+}
+
+CASE_TEST_EVENT_ON_EXIT(unit_test_event_on_exit_2, "unit_test_event_on_exit_1") {
+    if (2 == g_unit_test_event_on_start_status) {
+        g_unit_test_event_on_start_status = 3;
+    }
+}
