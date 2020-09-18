f0ac9b65832b30ff6858e3b71fd19f504daf0924
owent <admin@owent.net>
2020-09-18 16:43:32 +0800

    Update libunwind support

--- project/cmake/modules/FindLibunwind.cmake
+++ project/cmake/modules/FindLibunwind.cmake
@@ -14,9 +14,17 @@
 #   Libunwind_HAS_UNW_GETCONTEXT        - True if unw_getcontext() is found (optional).
 #   Libunwind_HAS_UNW_INIT_LOCAL        - True if unw_init_local() is found (optional).
 #   Libunwind_VERSION_STRING            - version number as a string (ex: "5.0.3")
+#
+# IMPORTED Targets
+# ^^^^^^^^^^^^^^^^
+# ::
+#
+#   Libunwind::libunwind               
+#
+# ::
 
 #=============================================================================
-# Copyright 2018 OWenT.
+# Copyright 2020 OWenT.
 #
 # Distributed under the OSI-approved BSD License (the "License");
 # see accompanying file Copyright.txt for details.
@@ -38,82 +46,141 @@ if(LIBUNWIND_ROOT)
   set(_LIBUNWIND_SEARCH_LIB PATHS ${LIBUNWIND_ROOT}/lib NO_DEFAULT_PATH)
 endif()
 
-find_path(Libunwind_INCLUDE_DIR NAMES libunwind.h ${_LIBUNWIND_SEARCH_INCLUDE} )
-if(NOT EXISTS "${Libunwind_INCLUDE_DIR}/unwind.h")
-  MESSAGE("Found libunwind.h but corresponding unwind.h is absent!")
-  SET(Libunwind_INCLUDE_DIR "")
+if(_LIBUNWIND_SEARCH_LIB AND EXISTS "${_LIBUNWIND_SEARCH_LIB}/pkgconfig/libunwind.pc")
+  find_package(PkgConfig)
+  if (PKG_CONFIG_FOUND)
+    pkg_check_modules(Libunwind "${_LIBUNWIND_SEARCH_LIB}/pkgconfig/libunwind.pc")
+  endif ()
+
+  if (NOT Libunwind_FOUND AND Libunwind_STATIC_FOUND)
+    set(Libunwind_FOUND ${Libunwind_STATIC_FOUND})
+    set(Libunwind_INCLUDE_DIRS ${Libunwind_STATIC_INCLUDE_DIRS})
+    set(Libunwind_LIBRARIES ${Libunwind_STATIC_LIBRARIES})
+    set(Libunwind_LIBRARY_DIRS ${Libunwind_STATIC_LIBRARY_DIRS})
+    set(Libunwind_LDFLAGS ${Libunwind_STATIC_LDFLAGS})
+    set(Libunwind_CFLAGS ${Libunwind_STATIC_CFLAGS})
+  endif ()
 endif()
 
-find_library(Libunwind_LIBRARY NAMES unwind ${_LIBUNWIND_SEARCH_LIB})
+if (NOT Libunwind_FOUND)
+  find_path(Libunwind_INCLUDE_DIRS NAMES libunwind.h ${_LIBUNWIND_SEARCH_INCLUDE} )
+  if(NOT EXISTS "${Libunwind_INCLUDE_DIRS}/unwind.h")
+    MESSAGE("Found libunwind.h but corresponding unwind.h is absent!")
+    set(Libunwind_INCLUDE_DIRS "")
+  endif()
 
-if(Libunwind_INCLUDE_DIR AND EXISTS "${Libunwind_INCLUDE_DIR}/libunwind-common.h")
-  file(STRINGS "${Libunwind_INCLUDE_DIR}/libunwind-common.h" Libunwind_HEADER_CONTENTS REGEX "#define UNW_VERSION_[A-Z]+\t[0-9]*")
+  find_library(Libunwind_LIBRARY NAMES unwind ${_LIBUNWIND_SEARCH_LIB})
+  find_library(Libunwind_LIBRARY_EXTRA NAMES unwind-${CMAKE_SYSTEM_PROCESSOR} ${_LIBUNWIND_SEARCH_LIB})
+  if (NOT Libunwind_LIBRARY_EXTRA AND ${CMAKE_SYSTEM_PROCESSOR} EQUAL ${CMAKE_HOST_SYSTEM_PROCESSOR})
+    find_library(Libunwind_LIBRARY_EXTRA NAMES unwind-generic ${_LIBUNWIND_SEARCH_LIB})
+  endif()
+  set(Libunwind_LIBRARIES ${Libunwind_LIBRARY_EXTRA} ${Libunwind_LIBRARY} CACHE FILEPATH "Path of libunwind libraries." FORCE)
+  get_filename_component(Libunwind_LIBRARY_DIRS ${Libunwind_LIBRARY} DIRECTORY CACHE)
+  include(FindPackageHandleStandardArgs)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(Libunwind REQUIRED_VARS  
+      Libunwind_INCLUDE_DIRS
+      Libunwind_LIBRARIES
+  )
+endif()
+
+
+if(Libunwind_INCLUDE_DIRS AND EXISTS "${Libunwind_INCLUDE_DIRS}/libunwind-common.h")
+  file(STRINGS "${Libunwind_INCLUDE_DIRS}/libunwind-common.h" Libunwind_HEADER_CONTENTS REGEX "#define UNW_VERSION_[A-Z]+\t[0-9]*")
 
   string(REGEX REPLACE ".*#define UNW_VERSION_MAJOR\t([0-9]*).*" "\\1" Libunwind_VERSION_MAJOR "${Libunwind_HEADER_CONTENTS}")
   string(REGEX REPLACE ".*#define UNW_VERSION_MINOR\t([0-9]*).*" "\\1" Libunwind_VERSION_MINOR "${Libunwind_HEADER_CONTENTS}")
   string(REGEX REPLACE ".*#define UNW_VERSION_EXTRA\t([0-9]*).*" "\\1" Libunwind_VERSION_EXTRA "${Libunwind_HEADER_CONTENTS}")
 
   if(Libunwind_VERSION_EXTRA)
-    set(Libunwind_VERSION_STRING "${Libunwind_VERSION_MAJOR}.${Libunwind_VERSION_MINOR}.${Libunwind_VERSION_EXTRA}")
-  else(not Libunwind_VERSION_EXTRA)
-    set(Libunwind_VERSION_STRING "${Libunwind_VERSION_MAJOR}.${Libunwind_VERSION_MINOR}")
+    set(Libunwind_VERSION_STRING "${Libunwind_VERSION_MAJOR}.${Libunwind_VERSION_MINOR}.${Libunwind_VERSION_EXTRA}" CACHE STRING "Version of libunwind" FORCE)
+  else()
+    set(Libunwind_VERSION_STRING "${Libunwind_VERSION_MAJOR}.${Libunwind_VERSION_MINOR}" CACHE STRING "Version of libunwind" FORCE)
   endif()
   unset(Libunwind_HEADER_CONTENTS)
 endif()
 
-if (Libunwind_LIBRARY)
-  find_library(Libunwind_LIBRARY_EXTRA NAMES unwind-${CMAKE_SYSTEM_PROCESSOR} ${_LIBUNWIND_SEARCH_LIB})
-  if (NOT Libunwind_LIBRARY_EXTRA AND ${CMAKE_SYSTEM_PROCESSOR} EQUAL ${CMAKE_HOST_SYSTEM_PROCESSOR})
-    find_library(Libunwind_LIBRARY_EXTRA NAMES unwind-generic ${_LIBUNWIND_SEARCH_LIB})
-  endif()
+if (Libunwind_LIBRARIES)
   include (CheckCSourceCompiles)
-  set(CMAKE_REQUIRED_QUIET_SAVE ${CMAKE_REQUIRED_QUIET})
-  set(CMAKE_REQUIRED_QUIET ${LibUnwind_FIND_QUIETLY})
   set(CMAKE_REQUIRED_LIBRARIES_SAVE ${CMAKE_REQUIRED_LIBRARIES})
-  set(CMAKE_REQUIRED_LIBRARIES ${Libunwind_LIBRARY} ${Libunwind_LIBRARY_EXTRA})
+  set(CMAKE_REQUIRED_LIBRARIES ${Libunwind_LIBRARIES})
   set(CMAKE_REQUIRED_INCLUDES_SAVE ${CMAKE_REQUIRED_INCLUDES})
-  set(CMAKE_REQUIRED_INCLUDES ${Libunwind_INCLUDE_DIR})
-  check_c_source_compiles("
-  #include <libunwind.h>
-  int main() {
-    unw_context_t unw_ctx;
-    unw_getcontext(&unw_ctx);
-    return 0;
-  }" Libunwind_HAS_UNW_GETCONTEXT)
-  check_c_source_compiles("
-  #include <libunwind.h>
-  int main() {
-    unw_context_t unw_ctx;
-    unw_cursor_t unw_cur;
-    unw_getcontext(&unw_ctx);
-    unw_init_local(&unw_cur, &unw_ctx);
-    return 0;
-  }" Libunwind_HAS_UNW_INIT_LOCAL)
-  set(CMAKE_REQUIRED_QUIET ${CMAKE_REQUIRED_QUIET_SAVE})
+  set(CMAKE_REQUIRED_INCLUDES ${Libunwind_INCLUDE_DIRS})
+  set(CMAKE_REQUIRED_FLAGS_SAVE ${CMAKE_REQUIRED_FLAGS})
+  set(CMAKE_REQUIRED_FLAGS ${Libunwind_CFLAGS})
+  set(CMAKE_REQUIRED_LINK_OPTIONS_SAVE ${CMAKE_REQUIRED_LINK_OPTIONS})
+  set(CMAKE_REQUIRED_LINK_OPTIONS ${Libunwind_LDFLAGS})
+  if (NOT Libunwind_HAS_UNW_GETCONTEXT)
+    check_c_source_compiles("
+    #include <libunwind.h>
+    int main() {
+      unw_context_t unw_ctx;
+      unw_getcontext(&unw_ctx);
+      return 0;
+    }" Libunwind_HAS_UNW_GETCONTEXT)
+  endif ()
+  if (NOT Libunwind_HAS_UNW_INIT_LOCAL)
+    check_c_source_compiles("
+    #include <libunwind.h>
+    int main() {
+      unw_context_t unw_ctx;
+      unw_cursor_t unw_cur;
+      unw_getcontext(&unw_ctx);
+      unw_init_local(&unw_cur, &unw_ctx);
+      return 0;
+    }" Libunwind_HAS_UNW_INIT_LOCAL)
+  endif ()
   set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES_SAVE})
   set(CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES_SAVE})
-  unset(CMAKE_REQUIRED_QUIET_SAVE)
+  set(CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS_SAVE})
+  set(CMAKE_REQUIRED_LINK_OPTIONS ${CMAKE_REQUIRED_LINK_OPTIONS_SAVE})
   unset(CMAKE_REQUIRED_LIBRARIES_SAVE)
   unset(CMAKE_REQUIRED_INCLUDES_SAVE)
+  unset(CMAKE_REQUIRED_FLAGS_SAVE)
+  unset(CMAKE_REQUIRED_LINK_OPTIONS_SAVE)
 endif ()
-
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Libunwind  REQUIRED_VARS  
-    Libunwind_INCLUDE_DIR
-    Libunwind_LIBRARY
-    VERSION_VAR Libunwind_VERSION_STRING
-)
 
 if (Libunwind_FOUND)
-  set(LIBUNWIND_FOUND ${Libunwind_FOUND})
-  set(Libunwind_LIBRARIES ${Libunwind_LIBRARY} ${Libunwind_LIBRARY_EXTRA})
+  if (NOT LIBUNWIND_FOUND)
+    set(LIBUNWIND_FOUND ${Libunwind_FOUND})
+  endif()
   set(LIBUNWIND_LIBRARIES ${Libunwind_LIBRARIES})
-  set(Libunwind_INCLUDE_DIRS ${Libunwind_INCLUDE_DIR})
   set(LIBUNWIND_INCLUDE_DIRS ${Libunwind_INCLUDE_DIRS})
+
+  if (NOT TARGET Libunwind::libunwind)
+    add_library(Libunwind::libunwind UNKNOWN IMPORTED)
+    set_target_properties(Libunwind::libunwind PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES ${Libunwind_INCLUDE_DIRS}
+    )
+    if (Libunwind_LIBRARIES)
+      list(GET Libunwind_LIBRARIES 0 Libunwind_LIBRARIES_LOCATION)
+      set_target_properties(Libunwind::libunwind PROPERTIES
+        IMPORTED_LINK_INTERFACE_LANGUAGES "C;CXX"
+        IMPORTED_LOCATION ${Libunwind_LIBRARIES_LOCATION}
+      )
+      set(Libunwind_LIBRARIES_LOCATION ${Libunwind_LIBRARIES})
+      list(REMOVE_AT Libunwind_LIBRARIES_LOCATION 0)
+      set_target_properties(Libunwind::libunwind PROPERTIES
+        INTERFACE_LINK_LIBRARIES ${Libunwind_LIBRARIES_LOCATION}
+      )
+      unset(Libunwind_LIBRARIES_LOCATION)
+    endif()
+    if (Libunwind_LDFLAGS)
+      set_target_properties(Libunwind::libunwind PROPERTIES
+        INTERFACE_LINK_OPTIONS ${Libunwind_LDFLAGS}
+      )
+    endif ()
+    if (Libunwind_CFLAGS)
+      set_target_properties(Libunwind::libunwind PROPERTIES
+        INTERFACE_COMPILE_OPTIONS ${Libunwind_CFLAGS}
+      )
+    endif ()
+  endif ()
+
+  mark_as_advanced(Libunwind_FOUND LIBUNWIND_FOUND Libunwind_INCLUDE_DIRS LIBUNWIND_INCLUDE_DIRS Libunwind_LIBRARIES LIBUNWIND_LIBRARIES 
+    Libunwind_LIBRARY_DIRS Libunwind_VERSION_STRING Libunwind_HAS_UNW_GETCONTEXT Libunwind_HAS_UNW_INIT_LOCAL)
 else()
-  unset(Libunwind_INCLUDE_DIR CACHE )
-  unset(Libunwind_LIBRARY CACHE )
+  unset(Libunwind_INCLUDE_DIRS CACHE )
+  unset(Libunwind_LIBRARIES CACHE )
+  unset(Libunwind_LIBRARY_DIRS CACHE )
   unset(Libunwind_VERSION_STRING CACHE )
 endif ()
-
-mark_as_advanced( Libunwind_INCLUDE_DIR Libunwind_LIBRARY Libunwind_LIBRARY_EXTRA )
