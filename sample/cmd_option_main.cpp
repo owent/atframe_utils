23b21794d7910fbeaa5902760bed6cf5328a5560
owent <admin@owent.net>
2020-09-19 11:28:03 +0800

    Fix sample codes

--- sample/cmd_option_main.cpp
+++ sample/cmd_option_main.cpp
@@ -79,11 +79,11 @@ static void on_error(util::cli::callback_param params) {
     std::stringstream args;
     for (util::cli::cmd_option_list::cmd_array_type::const_iterator iter = params.get_cmd_array().begin();
          iter != params.get_cmd_array().end(); ++iter) {
-        args << '"' << iter->first << '"';
+        args << " \"" << iter->first << '"';
     }
     for (size_t i = 0; i < params.get_params_number(); ++i) {
         if (params[i]) {
-            args << '"' << params[i]->to_cpp_string() << '"';
+            args << " \"" << params[i]->to_cpp_string() << '"';
         }
     }
 
