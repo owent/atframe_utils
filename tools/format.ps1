2d02e21909b83df39bbc11b565103280b293e381
owentou <owentou@tencent.com>
2020-12-26 19:25:10 +0800

    增加一些format通用性适配，增加cpack支持

--- tools/format.ps1
+++ tools/format.ps1
@@ -3,6 +3,6 @@ $PSDefaultParameterValues['*:Encoding'] = 'UTF-8'
 $OutputEncoding = [System.Text.UTF8Encoding]::new()
 
 $ALL_CMAKE_FILES = Get-ChildItem -Depth 0 -File -Include "*.cmake.in", "*.cmake", "CMakeLists.txt"
-$ALL_CMAKE_FILES += Get-ChildItem -Depth 0 -Directory -Exclude "build_jobs_*" | ForEach-Object -Process { Get-ChildItem -LiteralPath $_ -Recurse -File -Include "*.cmake.in", "*.cmake", "CMakeLists.txt" }
+$ALL_CMAKE_FILES += Get-ChildItem -Depth 0 -Directory -Exclude ".vs", ".vscode", ".clion", "build_jobs_*", "3rd_party", "third_party" | ForEach-Object -Process { Get-ChildItem -LiteralPath $_ -Recurse -File -Include "*.cmake.in", "*.cmake", "CMakeLists.txt" }
 
 cmake-format -i $ALL_CMAKE_FILES
--- tools/format.sh
+++ tools/format.sh
@@ -6,6 +6,8 @@ find .  -name 'build_jobs_*' -prune -o      \
         -name '.vscode' -prune -o           \
         -name '.vs' -prune -o               \
         -name '.clion' -prune -o            \
+        -name '3rd_party' -prune -o         \
+        -name 'third_party' -prune -o       \
         -type f                             \
         -name "*.cmake" -print              \
         -o -name "*.cmake.in" -print        \
