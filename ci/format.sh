b3851f092ef485f2381a966b6a57f81a010b6d8f
owent <admin@owent.net>
2021-05-20 10:18:48 +0800

    Upgrade ci to gcc-11
    Signed-off-by: owent <admin@owent.net>

--- ci/format.sh
+++ ci/format.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-find . -type f                                    \
-  -regex ".*third_party/packages/.*" -prune       \
-  -o -regex ".*third_party/install/.*" -prune     \
-  -o -regex ".*build_jobs_.*" -prune              \
-  -o -regex ".*project/cmake/toolset/.*" -prune   \
-  -o -name "*.cmake" -print                       \
-  -o -name "*.cmake.in" -print                    \
-  -o -name 'CMakeLists.txt' -print                \
+find . -type f                                      \
+  -regex ".*third_party/packages/.*" -prune         \
+  -o -regex ".*third_party/install/.*" -prune       \
+  -o -regex ".*build_jobs_.*" -prune                \
+  -o -regex ".*atframework/cmake-toolset/.*" -prune \
+  -o -name "*.cmake" -print                         \
+  -o -name "*.cmake.in" -print                      \
+  -o -name 'CMakeLists.txt' -print                  \
   | xargs cmake-format -i
 
-find . -type f                                    \
-  -regex ".*third_party/packages/.*" -prune       \
-  -o -regex ".*third_party/install/.*" -prune     \
-  -o -regex ".*build_jobs_.*" -prune              \
-  -o -regex ".*project/cmake/toolset/.*" -prune   \
-  -o -name "*.h" -print                           \
-  -o -name "*.hpp" -print                         \
-  -o -name "*.cxx" -print                         \
-  -o -name '*.cpp' -print                         \
-  -o -name '*.cc' -print                          \
-  -o -name '*.c' -print                           \
+find . -type f                                      \
+  -regex ".*third_party/packages/.*" -prune         \
+  -o -regex ".*third_party/install/.*" -prune       \
+  -o -regex ".*build_jobs_.*" -prune                \
+  -o -regex ".*atframework/cmake-toolset/.*" -prune \
+  -o -name "*.h" -print                             \
+  -o -name "*.hpp" -print                           \
+  -o -name "*.cxx" -print                           \
+  -o -name '*.cpp' -print                           \
+  -o -name '*.cc' -print                            \
+  -o -name '*.c' -print                             \
   | xargs -r -n 32 clang-format -i --style=file --fallback-style=none
