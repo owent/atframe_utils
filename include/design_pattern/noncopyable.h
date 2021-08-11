ccc5db550abf143ec4cda49d969630ac0c138644
owent <admin@owent.net>
2021-08-11 12:29:08 +0800

    Add result type(a simple alternative to std::variant)

--- include/design_pattern/noncopyable.h
+++ include/design_pattern/noncopyable.h
@@ -1,21 +1,10 @@
-/**
- * @file noncopyable.h
- * @brief 禁止复制基类和宏,继承禁止复制基类的子类不允许复制
- *
- *
- * @version 1.0
- * @author owent
- * @date 2012.02.21
- *
- * @history
- *      2017-01-21: using c++11 delete keyword in modern compiler
- *      2019-12-05: 增加 UTIL_DESIGN_PATTERN_NOCOPYABLE(类名) 宏，用于dllexport时自带的
- * util::design_pattern::noncopyable 未导出的问题 增加 UTIL_DESIGN_PATTERN_NOCOPYABLE(类名) 宏，用于dllexport时自带的
- * util::design_pattern::noncopyable 未导出的问题
- */
-
-#ifndef UTILS_DESIGNPATTERN_NONCOPYABLE_H
-#define UTILS_DESIGNPATTERN_NONCOPYABLE_H
+// Copyright 2021 atframework
+// Created by owent on 2012-02-21
+// @history
+//   2017-01-21: using c++11 delete keyword in modern compiler
+//   2019-12-05: 增加 UTIL_DESIGN_PATTERN_NOCOPYABLE(类名) 宏，用于dllexport时自带的
+// util::design_pattern::noncopyable 未导出的问题 增加 UTIL_DESIGN_PATTERN_NOCOPYABLE(类名) 宏，用于dllexport时自带的
+// util::design_pattern::noncopyable 未导出的问题
 
 #pragma once
 
@@ -45,5 +34,3 @@ class UTIL_SYMBOL_VISIBLE noncopyable {
  private:                                     \
   CLAZZ(const CLAZZ &) = delete;              \
   CLAZZ &operator=(const CLAZZ &) = delete;
-
-#endif
