5f1c23a7ee51caea5cd60e91a837ca9e38f8d9f7
owent <admin@owent.net>
2021-11-01 18:55:16 +0800

    Fix a compatitablity for gcc 4.8

--- include/nostd/type_traits.h
+++ include/nostd/type_traits.h
@@ -16,14 +16,45 @@ namespace nostd {
 template <std::size_t Len, class... Types>
 using aligned_union = std::aligned_union<Len, Types...>;
 #else
+template <std::size_t Len, class... Types>
+struct max_alignof_size_helper;
+
+template <std::size_t Len, class First, class... Types>
+struct max_alignof_size_helper<Len, First, Types...> {
+  static constexpr const std::size_t alignment_value = alignof(First) >
+                                                               max_alignof_size_helper<Len, Types...>::alignment_value
+                                                           ? alignof(First)
+                                                           : max_alignof_size_helper<Len, Types...>::alignment_value;
+};
+
+template <std::size_t Len>
+struct max_alignof_size_helper<Len> {
+  static constexpr const std::size_t alignment_value = Len;
+};
+
+template <std::size_t Len, class... Types>
+struct max_sizeof_size_helper;
+
+template <std::size_t Len, class First, class... Types>
+struct max_sizeof_size_helper<Len, First, Types...> {
+  static constexpr const std::size_t value = alignof(First) > max_sizeof_size_helper<Len, Types...>::value
+                                                 ? sizeof(First)
+                                                 : max_sizeof_size_helper<Len, Types...>::value;
+};
+
+template <std::size_t Len>
+struct max_sizeof_size_helper<Len> {
+  static constexpr const std::size_t value = Len;
+};
+
 // alignas is available from gcc 4.8, clang 3.0 and MSVC 19.0(2015)
 // alignof is available from gcc 4.5, clang 2.9 and MSVC 19.0(2015)
 template <std::size_t Len, class... Types>
 struct LIBATFRAME_UTILS_API_HEAD_ONLY aligned_union {
-  static constexpr std::size_t alignment_value = std::max({alignof(Types)...});
+  static constexpr const std::size_t alignment_value = max_alignof_size_helper<0, Types...>::alignment_value;
 
   struct type {
-    alignas(alignment_value) unsigned char _s[std::max({Len, sizeof(Types)...})];
+    alignas(sizeof(unsigned char[alignment_value])) unsigned char _s[max_sizeof_size_helper<0, Types...>::value];
   };
 };
 #endif
